7d3ea7e869d7c4c3dd5d8553d3972030654605ce
Santiago Persico <santiagopersico@gmail.com>
2024-10-15 17:24:36 +0200

    add support for lists

--- app/main.ts
+++ app/main.ts
@@ -1,25 +1,45 @@
 // Examples:
 // - decodeBencode("5:hello") -> "hello"
 // - decodeBencode("10:hello12345") -> "hello12345"
-function decodeBencode(bencodedValue: string): string | number {
-  /* This function is used to decode a bencoded string
-    The bencoded string is a string that is prefixed by the length of the string
-    **/
+// - decodeBencode("i123e") -> 123
+// - decodeBencode("l5:helloi52ee") -> -[“hello”,52]
+function decodeBencode(bencodedValue: string): {
+  value: number | string | any[];
+  lastIndexUsed: number;
+} {
+  const analyzer =
+    /(^(?<string_length>\d+):(?<string_text>.+))|(^i(?<integer_content>-?\d+)e$)|(^l(?<array_content>.*)e$)/;
 
-  // Check if the first character is a digit
-  if (!isNaN(parseInt(bencodedValue[0]))) {
-    const firstColonIndex = bencodedValue.indexOf(':');
-    if (firstColonIndex === -1) {
-      throw new Error('Invalid encoded value');
+  const matches = bencodedValue.match(analyzer);
+  console.log('decodeBencode =>', bencodedValue, '=> ', matches?.groups);
+
+  if (!matches || !matches.groups) throw new Error('Invalid encoded value');
+
+  const { string_length, string_text, integer_content, array_content } =
+    matches?.groups;
+
+  if (array_content) {
+    const decodedList = [];
+    let lastIndexUsed = 0;
+    while (lastIndexUsed < array_content.length) {
+      const decodedValue = decodeBencode(
+        array_content.substring(lastIndexUsed)
+      );
+      decodedList.push(decodedValue.value);
+      lastIndexUsed += decodedValue.lastIndexUsed;
     }
-    return bencodedValue.substring(firstColonIndex + 1);
-  } else if (
-    bencodedValue[0] === 'i' &&
-    bencodedValue[bencodedValue.length - 1] === 'e'
-  ) {
-    return Number(bencodedValue.substring(1, bencodedValue.length - 1));
+    return { value: decodedList, lastIndexUsed };
+  } else if (integer_content) {
+    return {
+      value: parseInt(integer_content),
+      lastIndexUsed: integer_content.length + 2,
+    };
   } else {
-    throw new Error('Only strings are supported at the moment');
+    const actualStringLength = parseInt(string_length);
+    return {
+      value: string_text.substring(0, actualStringLength),
+      lastIndexUsed: string_length.length + 1 + actualStringLength,
+    };
   }
 }
 
@@ -29,7 +49,7 @@ const bencodedValue = args[3];
 if (args[2] === 'decode') {
   try {
     const decoded = decodeBencode(bencodedValue);
-    console.log(JSON.stringify(decoded));
+    console.log(JSON.stringify(decoded.value));
   } catch (error) {
     console.error(error.message);
   }
