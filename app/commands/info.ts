7790e0f38bbc90328fecb10123da6299b5ebf7cd
Santiago Persico <santiagopersico@gmail.com>
2024-10-17 20:11:01 +0200

    add infohash calc

--- app/commands/info.ts
+++ app/commands/info.ts
@@ -1,15 +1,10 @@
 import { promises as fs } from 'fs';
+import { createHash } from 'crypto';
 import bencode from 'bencode';
 
 async function readFileContent(filename: string): Promise<Buffer> {
   try {
-    // Read the file as a Buffer
     const buffer = await fs.readFile(filename);
-
-    // You can try different encodings based on the file content:
-    // UTF-8 if the file mostly contains text and valid characters
-    // Latin1 (ISO-8859-1) to preserve the byte values for non-UTF-8 characters
-
     return buffer;
   } catch (error) {
     if (error instanceof Error)
@@ -18,18 +13,29 @@ async function readFileContent(filename: string): Promise<Buffer> {
   }
 }
 
+function getInfoHash(info: any) {
+  const hasher = createHash('sha1');
+  const encodedInfo = new Uint8Array(bencode.encode(info));
+  hasher.update(encodedInfo);
+  return hasher.digest('hex');
+}
+
 export async function torrentInfo(filename: string) {
   try {
     const fileContents = await readFileContent(filename);
     if (!fileContents) throw new Error('Empty torrent file');
 
-    const decoded = bencode.decode(fileContents);
-    if (!decoded?.info || !decoded?.announce)
+    const metadata = bencode.decode(fileContents);
+    if (!metadata?.info || !metadata?.announce)
       throw new Error('Error decoding the torrent file');
-    const announce = new TextDecoder().decode(decoded['announce']);
+    const announce = new TextDecoder().decode(metadata['announce']);
+    const hash = getInfoHash(metadata.info);
+
     console.log(`Tracker URL: ${announce}`);
-    console.log(`Length: ${decoded.info.length}`);
-    return decoded;
+    console.log(`Length: ${metadata.info.length}`);
+    console.log(`Info Hash: ${hash}`);
+
+    return { metadata, hash };
   } catch (error: unknown) {
     if (error instanceof Error) console.error(error?.message);
     throw error;
