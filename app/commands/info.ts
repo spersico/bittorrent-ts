2ed04a8eafac1ee73a574ff4298b3d1f120a4f45
Santiago Persico <santiagopersico@gmail.com>
2024-10-17 17:12:41 +0200

    fix torrentInfo return value

--- app/commands/info.ts
+++ app/commands/info.ts
@@ -14,14 +14,15 @@ async function readFileContent(filename: string): Promise<string> {
 export async function torrentInfo(filename: string) {
   try {
     const fileContents = await readFileContent(filename);
+    if (!fileContents) throw new Error('Empty torrent file');
+
     const decoded = decodeBencode(fileContents) as BencodeResult<BencodeDict>;
     if (!decoded?.value?.info || !decoded?.value?.announce)
-      throw new Error('Invalid torrent file');
-    const result = {
-      'Tracker URL': decoded.value['announce'],
-      Length: (decoded.value.info as BencodeDict).length,
-    };
-    console.log(JSON.stringify(result));
+      throw new Error('Error decoding the torrent file');
+
+    console.log(`Tracker URL: ${decoded.value['announce']}`);
+    console.log(`Length: ${(decoded.value.info as BencodeDict).length}`);
+    return decoded.value;
   } catch (error: unknown) {
     if (error instanceof Error) console.error(error?.message);
     throw error;
