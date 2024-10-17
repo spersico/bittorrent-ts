6a32c190fbbbae8a82c87e6cb7916029e4adb553
Santiago Persico <santiagopersico@gmail.com>
2024-10-17 13:13:51 +0200

    working dictionary I think

--- app/main.ts
+++ app/main.ts
@@ -57,15 +57,20 @@ function decodeBencode(bencodedValue: string): {
   lastIndexUsed: number;
 } {
   const analyzer =
-    /(^(?<string_length>\d+):(?<string_text>.+))|(^i(?<integer_content>-?\d+)e)|(^l(?<array_content>.*)e)/;
+    /(?:^(?<string_length>\d+):(?<string_text>.+))|(?:^i(?<integer_content>-?\d+)e)|(?:^l(?<array_content>.*)e)|(?:^d(?<dictionary_content>.*)e)/;
 
   const matches = bencodedValue.match(analyzer);
   //   console.log('decodeBencode =>', bencodedValue, '=> ', matches?.groups);
 
   if (!matches || !matches.groups) throw new Error('Invalid encoded value');
 
-  const { string_length, string_text, integer_content, array_content } =
-    matches?.groups;
+  const {
+    string_length,
+    string_text,
+    integer_content,
+    array_content,
+    dictionary_content,
+  } = matches?.groups;
 
   if (typeof integer_content === 'string') {
     return {
@@ -78,7 +83,7 @@ function decodeBencode(bencodedValue: string): {
       value: string_text.substring(0, actualStringLength),
       lastIndexUsed: string_length.length + 1 + actualStringLength,
     };
-  } else {
+  } else if (typeof array_content === 'string') {
     const decodedList = [];
     let lastIndexUsed = 0;
     // console.log(`🐛 | decodeBencode | ARRAY:`, array_content);
@@ -92,12 +97,6 @@ function decodeBencode(bencodedValue: string): {
       const itWasArrayElement =
         array_content[lastIndexUsed] === 'e' &&
         array_content[lastIndexUsed - 1] === 'e';
-      //   console.log(`🐛 | decodeBencode | itWasArrayElement:`, {
-      //     itWasArrayElement,
-      //     array_content,
-      //     lastIndexUsed,
-      //     decodedValue,
-      //   });
 
       if (itWasArrayElement) {
         lastIndexUsed += 1;
@@ -110,7 +109,57 @@ function decodeBencode(bencodedValue: string): {
       }
     }
     return { value: decodedList, lastIndexUsed: lastIndexUsed + 2 };
+  } else if (typeof dictionary_content === 'string') {
+    const decodedList = [];
+    let lastIndexUsed = 0;
+    // console.log(`🐛 | decodeBencode | ARRAY:`, array_content);
+
+    while (lastIndexUsed < dictionary_content.length) {
+      const decodedValue = decodeBencode(
+        dictionary_content.substring(lastIndexUsed)
+      );
+      lastIndexUsed += decodedValue.lastIndexUsed;
+
+      const itWasAcomplexElement =
+        dictionary_content[lastIndexUsed] === 'e' &&
+        dictionary_content[lastIndexUsed - 1] === 'e';
+
+      if (itWasAcomplexElement && decodedList.length > 0) {
+        lastIndexUsed += 1;
+        decodedList.push(decodedValue.value);
+
+        return {
+          value: sortAndBuildDictionary(decodedList),
+          lastIndexUsed: lastIndexUsed + 1,
+        };
+      } else {
+        decodedList.push(decodedValue.value);
+      }
+    }
+    return {
+      value: sortAndBuildDictionary(decodedList),
+      lastIndexUsed: lastIndexUsed + 2,
+    };
+  } else {
+    throw new Error('Invalid encoded value');
   }
+}
+
+function sortAndBuildDictionary(dictionary: any[]): any {
+  const unsortedResults = {} as Record<string, any>;
+  for (let i = 0; i < dictionary.length; i += 2) {
+    unsortedResults[dictionary[i]] = dictionary[i + 1];
+  }
+
+  const sortedDictionary = Object.entries(unsortedResults)
+    .sort((a, b) => a[0].localeCompare(b[0]))
+    .reduce((acc, keyValuePair) => {
+      console.log(`🐛 | .reduce | keyValuePair:`, keyValuePair);
+      acc[keyValuePair[0]] = keyValuePair[1];
+      return acc;
+    }, {} as Record<string, any>);
+
+  return sortedDictionary;
 }
 
 const args = process.argv;
